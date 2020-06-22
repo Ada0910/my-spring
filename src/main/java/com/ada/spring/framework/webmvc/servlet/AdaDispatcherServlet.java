0ac06c811a9b433e64f90dfa56767b18a34f41be
Ada0910 <1355948107@qq.com>
2020-06-22 23:43:32 +0800

    配置dispatchServlet

---
+++ src/main/java/com/ada/spring/framework/webmvc/servlet/AdaDispatcherServlet.java
@@ -0,0 +1,10 @@
+package com.ada.spring.framework.webmvc.servlet;
+
+/**
+ * @author Ada
+ * @ClassName :AdaDispatcherServlet
+ * @date 2020/6/22 23:42
+ * @Description:
+ */
+public class AdaDispatcherServlet {
+}
