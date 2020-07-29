2540d93f9ae4bdcbea81ffb012e722e3d3006fab
Ada0910 <1355948107@qq.com>
2020-07-29 23:39:33 +0800

    Aop

--- src/main/java/com/ada/spring/framework/aop/AdaJdkDynamicAopProxy.java
+++ src/main/java/com/ada/spring/framework/aop/AdaJdkDynamicAopProxy.java
@@ -1,6 +1,12 @@
 package com.ada.spring.framework.aop;
 
+import com.ada.spring.framework.aop.aspect.AdaAdvice;
 import com.ada.spring.framework.aop.support.AdaAdvisedSupport;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
 
 /**
  * @author Ada
@@ -8,11 +14,31 @@ import com.ada.spring.framework.aop.support.AdaAdvisedSupport;
  * @date 2020/7/27 23:28
  * @Description:
  */
-public class AdaJdkDynamicAopProxy {
+public class AdaJdkDynamicAopProxy implements InvocationHandler {
+    private AdaAdvisedSupport config;
+
     public AdaJdkDynamicAopProxy(AdaAdvisedSupport config) {
+        this.config = config;
     }
 
     public Object getProxy() {
+        return Proxy.newProxyInstance(this.getClass().getClassLoader(), this.config.getTargetClass().getInterfaces(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        Map<String, AdaAdvice> advices = this.config.getAdvice(method, this.config.getTargetClass());
+        advices.get("before");
+        try {
+            method.invoke(this.config.getTarget(), args);
+        } catch (Exception e) {
+            advices.get("afterThrowing");
+            e.printStackTrace();
+            throw e;
+        }
+
+        advices.get("after");
         return null;
     }
 }
--- src/main/java/com/ada/spring/framework/aop/support/AdaAdvisedSupport.java
+++ src/main/java/com/ada/spring/framework/aop/support/AdaAdvisedSupport.java
@@ -1,6 +1,12 @@
 package com.ada.spring.framework.aop.support;
 
+import com.ada.spring.framework.aop.aspect.AdaAdvice;
 import com.ada.spring.framework.aop.config.AdaAopConfig;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * @author Ada
@@ -9,17 +15,67 @@ import com.ada.spring.framework.aop.config.AdaAopConfig;
  * @Description:
  */
 public class AdaAdvisedSupport {
+    private AdaAopConfig config;
+    private Class targetClass;
+    private Object target;
+    private Pattern pointCutClassPattern;
+    private Map<Method, Map<String, AdaAdvice>> methodCache;
+
     //解析读取出来的配置信息
     public AdaAdvisedSupport(AdaAopConfig config) {
+        this.config = config;
+    }
+
+
+    public Class getTargetClass() {
+        return targetClass;
+    }
+
+    public void setTargetClass(Class targetClass) {
+        this.targetClass = targetClass;
+        parse();
+    }
+
+    private void parse() {
+        String pointCut = config.getPointCut()
+                .replaceAll("\\.", "\\\\.")
+                .replaceAll("\\\\.\\*", ".*")
+                .replaceAll("\\(", "\\\\(")
+                .replaceAll("\\)", "\\\\)");
+
+        String pointCutForClassRegex = pointCut.substring(0, pointCut.lastIndexOf("\\(") - 4);
+        //提取class的全名
+        pointCutClassPattern = Pattern.compile("Class " +
+                pointCutForClassRegex.substring(pointCutForClassRegex.lastIndexOf(" " + 1)));
+        try {
+            //保存方法和通知的关系
+            methodCache = new HashMap<>();
+            Pattern pointCutPattern = Pattern.compile(pointCut);
+            Class aspectClass = Class.forName(this.config.getAspectClass());
+            Map<String, Method> aspectMethods = new HashMap<>();
+            for (Method method : aspectClass.getMethods()) {
+                aspectMethods.put(method.getName(), method);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public boolean pointCutMatch() {
-        return false;
+        return pointCutClassPattern.matcher(this.targetClass.getName()).matches();
     }
 
-    public void setTargetClass(Class<?> clazz) {
+    public Object getTarget() {
+        return target;
     }
 
-    public void setTarget(Object instance) {
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public Map<String, AdaAdvice> getAdvice(Method method, Class targetClass) {
+        return null;
     }
 }
